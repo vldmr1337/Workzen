f6c0291a561c134a1ee057dd0b8570dd82913999
vldmr1337 <netok812@gmail.com>
2024-07-13 01:44:08 -0300

    Ajuste para permitir a alteração de imagem no método put da rota /me, utilizado o middleware multer para funcionamento.

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const xpController = require('../controllers/xpController');
 const userController = require('../controllers/userController');
 const authenticate = require('../middlewares/authenticate');
+const upload = require('../middlewares/multer');
+
 
 router.get('/me', authenticate, userController.getProfile);
-router.put('/me', authenticate, userController.updateProfile);
+router.put('/me', authenticate, upload.single('image'), userController.updateProfile);
 router.delete('/me', authenticate, userController.deleteProfile);
 router.post('/me/xp', authenticate, xpController.createExperience);
 router.get('/me/xp', authenticate, xpController.getExperiences);
