300a43f263d86422dfd1edb2cfa920610f156274
vldmr1337 <netok812@gmail.com>
2024-07-17 14:56:14 -0300

    Adicionado novo arquivo de configuração do nodemailer.

---
+++ config/mail.js
@@ -0,0 +1,13 @@
+const nodemailer = require('nodemailer');
+require("dotenv").config();
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP,
+  port: process.env.SMTP_PORT,
+  secure: true,
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASSWORD,
+  },
+});
+
+module.exports = transporter;
