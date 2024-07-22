1efd314ca614499b95466c5d24368b57e75e4534
vldmr1337 <netok812@gmail.com>
2024-07-22 08:22:27 -0300

    Adicionada nova biblioteca para validação de CNPJ e implementada no controller de empresa.

--- controllers/empresaController.js
+++ controllers/empresaController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const secret = process.env.JWT_SECRET;
+const cnpj = require('@fnando/cnpj');
 
 exports.login = async (req, res) => {
   try {
@@ -25,41 +26,54 @@ exports.login = async (req, res) => {
   }
 };
 
+
 exports.register = async (req, res) => {
   try {
-    const { email, password, cnpj, ramo_atividade, nome } = req.body;
-    if (!email || !password || !cnpj || !ramo_atividade || !nome) {
-      return res
-        .status(400)
-        .json({ message: "Todos os campos são obrigatórios." });
+    const { email, password, cnpj: cnpjValue, ramo_atividade, nome } = req.body;
+
+    if (!email || !password || !cnpjValue || !ramo_atividade || !nome) {
+      return res.status(400).json({ message: "Todos os campos são obrigatórios." });
     }
 
+    // Verifica se o CNPJ é válido
+    if (!cnpj.isValid(cnpjValue)) {
+      return res.status(400).json({ message: 'CNPJ inválido.' });
+    }
+
+    // Verifica se o e-mail já está registrado
     const existingEmpresa = await Empresa.findOne({ email });
     if (existingEmpresa) {
       return res.status(400).json({ message: "E-mail já registrado." });
     }
 
-    const existingCNPJ = await Empresa.findOne({ cnpj });
+    // Verifica se o CNPJ já está registrado
+    const existingCNPJ = await Empresa.findOne({ cnpj: cnpjValue });
     if (existingCNPJ) {
       return res.status(400).json({ message: "CNPJ já registrado." });
     }
 
+    // Cria o hash da senha
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
+    // Cria a nova empresa
     const empresa = new Empresa({
       email,
       password: hashedPassword,
-      cnpj,
+      cnpj: cnpjValue,
       ramo_atividade,
       nome,
     });
 
+    // Salva a empresa no banco de dados
     await empresa.save();
+
+    // Gera o token JWT
     const token = jwt.sign({ id: empresa._id, userType: "empresa" }, secret, {
       expiresIn: "1h",
     });
 
+    // Responde com a empresa criada e o token
     res.status(201).json({ empresa, token });
   } catch (error) {
     console.error(error);
