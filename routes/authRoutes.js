3aaffbef09ece2e0c9211ae634650b90453347fd
vldmr1337 <netok812@gmail.com>
2024-07-22 16:39:20 -0300

    Adicionado redirecionamento para front-end passando o token.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,6 +13,8 @@ router.get('/google/callback', passport.authenticate('google', { session: false 
   // Aqui você pode gerar um token JWT e enviá-lo ao cliente
   const token = jwt.sign({ id: req.user._id, userType: 'usuario' }, process.env.JWT_SECRET, { expiresIn: '1h' });
   res.json({ token });
+  res.redirect(`https://tcc-react-three.vercel.app/magic?token=${token}`);
 });
+
 
 module.exports = router;
